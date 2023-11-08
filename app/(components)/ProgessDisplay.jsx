7e6095a58763f45c3c6f9e080db9297fcbe50e6c
Ehueriken Samuel Obuse <59874300+sobuse@users.noreply.github.com>
2023-11-08 20:16:05 +0100

    created more component

---
+++ app/(components)/ProgessDisplay.jsx
@@ -0,0 +1,13 @@
+
+const ProgessDisplay = () => {
+  return (
+    <div className="w-full bg-gray-200 rounded-full h-2.5">
+        <div className="bg-blue-600 h-2.5 round-full"
+        style={{width:"75%"}}>
+
+        </div>
+    </div>
+  )
+}
+
+export default ProgessDisplay
---
+++ app/(components)/StatusDisplay.jsx
@@ -0,0 +1,10 @@
+
+const StatusDisplay = () => {
+  return (
+    <span className="inline-block rounded-full px-2 py-1 text-xs text-gray-700 bg-green-200" > 
+        Done
+    </span>
+  )
+}
+
+export default StatusDisplay
