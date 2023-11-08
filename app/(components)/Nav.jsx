7d0282ec1c63197cad3ab043e3971f4310e517f9
Ehueriken Samuel Obuse <59874300+sobuse@users.noreply.github.com>
2023-11-08 14:47:54 +0100

    Applied design to my nav bar

--- app/(components)/Nav.jsx
+++ app/(components)/Nav.jsx
@@ -1,6 +1,23 @@
+import { faHome, faTicket } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import Link from "next/link"
+
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <nav className="flex justify-between bg-nav">
+        <div className="flex item-center space-x-4">
+            <Link href="/">
+                <FontAwesomeIcon icon={faHome} className="icon"></FontAwesomeIcon>
+            </Link>
+
+            <Link href="/TicketPage/new">
+                <FontAwesomeIcon icon={faTicket} className="icon"></FontAwesomeIcon>
+            </Link>
+        </div>
+        <div>
+            <p className="text-default-text">Ehuerikenbaba@gmaol.com</p>
+        </div>
+    </nav>
   )
 }
 
