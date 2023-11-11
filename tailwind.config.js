653a5df66e93af7374058f450165a2b986a56f57
Ehueriken Samuel Obuse <59874300+sobuse@users.noreply.github.com>
2023-11-11 14:36:54 +0100

    updated design

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,9 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      borderRadius: {
+        xl: '1.5rem', 
+      },
       colors: {
         nav: "#18222f",
         page:"#2b3441",
