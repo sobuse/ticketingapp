569fd3bef8dc92afc27e58d022a630b8863a60a9
Ehueriken Samuel Obuse <59874300+sobuse@users.noreply.github.com>
2023-11-13 18:01:02 +0100

    removed hard coded values and made the page dyanamic

--- app/(components)/TicketCard.jsx
+++ app/(components)/TicketCard.jsx
@@ -1,31 +1,32 @@
 import React from 'react'
 import DeleteBlock from './DeleteBlock'
-import ProgessDisplay from './ProgessDisplay'
+import ProgessDisplay from './ProgressDisplay'
 import ProrityDisplay from './ProrityDisplay'
 import StatusDisplay from './StatusDisplay'
 
-const TicketCard = () => {
+const TicketCard = ({ticket}) => {
   return (
     <div className='flex flex-col bg-card hover:bg-card-hover rounded-md shadow-lg p-3 m-2'>
         <div className='flex mb-3'>
-        <ProrityDisplay/>
+        <ProrityDisplay priority={ticket.priority}/>
 
         <div className='ml-auto'>
         <DeleteBlock/>
         </div>
 
         </div>
-        <h4>Ticket Title</h4>
+        <h4>{ticket.title}</h4>
         <hr className='h-px border-0 bg-page mb-2'/>
-        <p className='whitwspace-pre-wrap'>This is the ticket description! Please do this ticket</p>
+        <p className='whitwspace-pre-wrap'>{ticket.description}</p>
         <div className='flex-grow'></div>
         <div className='flex mt-2'>
         <div className='flex flex-col'>
-        <p className='text-xs my-1'>11/9/23 1:01PM</p>
-        <ProgessDisplay/>
+        <p className='text-xs my-1'>{ticket.createdAt}
+        </p>
+        <ProgessDisplay progress={ticket.progress}/>
         </div>
         <div className='ml-auto flex item-end'>
-        <StatusDisplay/> 
+        <StatusDisplay status={ticket.status}/> 
         </div>
         </div>
     </div>
--- app/page.jsx
+++ app/page.jsx
@@ -1,13 +1,46 @@
 import TicketCard from "./(components)/TicketCard"
 
-const Dashboard = () => {
+
+const getTickets = async () => {
+  try {
+    const res = await fetch("http://localhost:3000/api/Tickets", {
+      cache: "no-store",
+    });
+
+    if (!res.ok) {
+      throw new Error(`Failed to fetch tickets. Status: ${res.status}`);
+    }
+
+    const data = await res.json();
+    return data || {}; // Return an empty object if the response is empty
+  } catch (error) {
+    console.log("Failed to get tickets", error);
+    return {};
+  }
+};
+
+
+const Dashboard = async() => {
+  const {tickets} = await getTickets();
+
+  const uniqueCategory = [
+    ...new Set(tickets?.map(({category})=> category)),
+  ]
+
   return (
     <div className="p-5">
-      <div className="lg:grid grid-cols-2 xl:grid-cols-4">
-      <TicketCard/>
-      <TicketCard/>
-      <TicketCard/>
-      <TicketCard/>
+      <div>
+        {tickets && uniqueCategory?.map((uniqueCategory, categoryIndex) => ( 
+        <div Key= {categoryIndex} className="mb-4">
+          <h2>{uniqueCategory}</h2>
+          <div className="lg:grid grid-cols-2 xl:grid-cols-4"> 
+            {tickets.filter((ticket)=> ticket.category === uniqueCategory).map((filteredTicket, _index)=>(
+              <TicketCard id={_index} key={_index} ticket={filteredTicket}/>
+            ))}
+      
+      </div>
+        </div>))}
+      
       </div>
     </div>
   )
