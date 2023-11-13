956c24bd3f9ffdc463ad610dbd2cf32746f6281f
Ehueriken Samuel Obuse <59874300+sobuse@users.noreply.github.com>
2023-11-13 18:02:46 +0100

    Update route.js

--- app/api/Tickets/route.js
+++ app/api/Tickets/route.js
@@ -1,17 +1,33 @@
-import Ticket from "@/app/(models)/Ticket"
+import Ticket from "../../(models)/Ticket";
 import { NextResponse } from "next/server";
 
 
-export async function POST(req){
-    console.log("POST ran")
-    try {
-        const body = await req.json();
-        const ticketData = body.formData;
-        await Ticket.create(ticketData);
-
-        return NextResponse.json({message: "Ticket Created"}, {status: 201})
-        
-    } catch (error) {
-        return NextResponse.json({message: "Error", error}, {status: 500})
+export async function POST(req) {
+  const response = new NextResponse();
+  const body = await req.json();
+    if (!body) {
+      console.error("Empty or invalid JSON response");
+      return response.json({ message: "Error", error: "Invalid JSON" }, { status: 500 });
     }
+    const ticketData = body.formData;
+  try {
+    await Ticket.create(ticketData);
+    return  response.json({ message: "Ticket Created" }, { status: 201 });
+  } catch (error) {
+    console.error("Error in POST request:", error);
+
+    return  response.json({ message: "Error", error }, { status: 500 });
+  }
+}
+
+export async function GET() {
+  const response = new NextResponse();
+  try {
+    const tickets = await Ticket.find();
+    return response.json({ tickets }, { status: 200 });
+  } catch (error) {
+    console.error("Error in GET request:", error);
+
+    return response.json({ message: "Error", error }, { status: 500 });
+  }
 }
