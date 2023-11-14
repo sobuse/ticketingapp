d76239c400b142c037c5dc360ab2e75a9d49e5b5
Ehueriken Samuel Obuse <59874300+sobuse@users.noreply.github.com>
2023-11-14 21:22:11 +0100

    add logs to trace error

--- app/page.jsx
+++ app/page.jsx
@@ -1,23 +1,28 @@
 import TicketCard from "./(components)/TicketCard"
 
 
+
 const getTickets = async () => {
   try {
     const res = await fetch("http://localhost:3000/api/Tickets", {
       cache: "no-store",
     });
+    console.log('Response:', res);
 
     if (!res.ok) {
+      console.error(`Failed to fetch tickets. Status: ${res.status}`);
+      console.error(await res.text()); // Log the response text for debugging
       throw new Error(`Failed to fetch tickets. Status: ${res.status}`);
     }
 
     const data = await res.json();
-    return data || {}; // Return an empty object if the response is empty
+    return data || {};
   } catch (error) {
-    console.log("Failed to get tickets", error);
+    console.error("Failed to get tickets", error);
     return {};
   }
 };
+
 
 
 const Dashboard = async() => {
@@ -31,7 +36,8 @@ const Dashboard = async() => {
     <div className="p-5">
       <div>
         {tickets && uniqueCategory?.map((uniqueCategory, categoryIndex) => ( 
-        <div Key= {categoryIndex} className="mb-4">
+        <div key={categoryIndex} className="mb-4">
+
           <h2>{uniqueCategory}</h2>
           <div className="lg:grid grid-cols-2 xl:grid-cols-4"> 
             {tickets.filter((ticket)=> ticket.category === uniqueCategory).map((filteredTicket, _index)=>(
