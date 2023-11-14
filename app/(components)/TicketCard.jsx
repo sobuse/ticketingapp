a6baa3c45c8546d277ac3531ac2728e4c368653e
Ehueriken Samuel Obuse <59874300+sobuse@users.noreply.github.com>
2023-11-14 21:21:44 +0100

    formated date time and made the delete icon dynamic

--- app/(components)/TicketCard.jsx
+++ app/(components)/TicketCard.jsx
@@ -5,13 +5,28 @@ import ProrityDisplay from './ProrityDisplay'
 import StatusDisplay from './StatusDisplay'
 
 const TicketCard = ({ticket}) => {
+
+  const formatTimeStamp = (timestamp) =>{
+    const option = {
+      year: "numeric",
+      month: "2-digit",
+      day: "2-digit",
+      hour: "2-digit",
+      minute: "2-digit",
+      hour12: true
+    };
+    const date = new Date(timestamp);
+    const formattedDate = date.toLocaleString("en-US", option);
+
+    return formattedDate;
+  }
   return (
     <div className='flex flex-col bg-card hover:bg-card-hover rounded-md shadow-lg p-3 m-2'>
         <div className='flex mb-3'>
         <ProrityDisplay priority={ticket.priority}/>
 
         <div className='ml-auto'>
-        <DeleteBlock/>
+        <DeleteBlock id={ticket._id}/>
         </div>
 
         </div>
@@ -21,7 +36,7 @@ const TicketCard = ({ticket}) => {
         <div className='flex-grow'></div>
         <div className='flex mt-2'>
         <div className='flex flex-col'>
-        <p className='text-xs my-1'>{ticket.createdAt}
+        <p className='text-xs my-1'>{formatTimeStamp(ticket.createdAt)}
         </p>
         <ProgessDisplay progress={ticket.progress}/>
         </div>
